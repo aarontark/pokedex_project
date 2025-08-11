6de6273b5510417da9537460af6cdc8bd512eb56
aarontark <tarkington.aaron@gmail.com>
2025-08-11 12:52:08 -0500

    created functionality for loading screen

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const cardContainer = document.querySelector('.cards-display');
+const regionBtnsContainer = document.querySelector('.region-btns');
+const loadingContainer = document.querySelector('.loading-container');
 const types = ['fire', 'grass', 'electric', 'water', 'ground', 'rock', 'fairy', 'poison', 'bug', 'dragon', 'psychic', 'flying', 'fighting', 'normal', 'ghost', 'dark', 'steel', 'ice']
 const regionBtns = Array.from(document.querySelector('.region-btns').children);
 const regions = [[1, 151], [152, 251], [252, 386], [387, 493], [494, 649], [650, 721], [722, 809], [810, 905], [906, 1025]];
@@ -27,6 +29,9 @@ const renderData = (pokeData, pokemonTypesID) => {
 
 // separate fetching data and creating html in two separate functions later
 async function fetchPokeData(region) {
+    cardContainer.style.display = 'none';
+    regionBtnsContainer.style.display = 'none';
+    loadingContainer.style.display = 'flex';
     cardContainer.innerHTML = '';
     let pokemonTypesID = region[0]
     for(i=region[0]; i <= region[1]; i++) {
@@ -35,10 +40,12 @@ async function fetchPokeData(region) {
         renderData(pokeData, pokemonTypesID);
         pokemonTypesID++;
     }
+    cardContainer.style.display = 'flex';
+    regionBtnsContainer.style.display = 'flex';
+    loadingContainer.style.display = 'none';
 }
 
 for (let i = 0; i < regions.length; i++) {
-    console.log(regions[i]);
     regionBtns[i].addEventListener('click', () => {
         fetchPokeData(regions[i]);
     })
